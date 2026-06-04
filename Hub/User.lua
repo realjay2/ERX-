42d76e035d8ed1c8958e5a76a7a083a05aa6fc8b
realjay2 <ajtheprince2020@gmail.com>
2026-06-03 19:26:30 -0700

    Update User.lua

--- Hub/User.lua
+++ Hub/User.lua
@@ -1,3 +1,17 @@
--- Soon
-local soon = "soon"
-print(soon)
+local Scripts = {
+    "https://erx.luauth.org/Open_Sourced/Car_Fly.lua",
+    "https://erx.luauth.org/Open_Sourced/Suspension_Mod.lua",
+    "https://erx.luauth.org/Open_Sourced/CarJump.lua"
+}
+
+for _, url in ipairs(Scripts) do
+    local ok, err = pcall(function()
+        loadstring(game:HttpGet(url, true))()
+        print("[ERX²] Loaded Addons")
+    end)
+
+    if not ok then
+        warn("[ERX] Failed to load:", url)
+        warn(err)
+    end
+end
